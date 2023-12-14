6119e0d58f3711c3b31772db984abb95ef7683d0
mswitas <mswitas@gmail.com>
2023-12-14 16:35:22 +0100

    add temperament

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import { fetchBreeds, fetchCatByBreed } from "./js/cat-api";
 
 const selectElement = document.querySelector('.breed-select');
 
+
+
 fetchBreeds();
 
 selectElement.addEventListener('change', (event) => {
